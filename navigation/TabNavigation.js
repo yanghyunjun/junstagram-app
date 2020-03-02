0130d48a96511a071b006b3422c72a1cdb494631
yanghyunjun <akakak231@gmail.com>
2020-03-02 22:56:28 +0900

    [Del] test

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,23 +1,24 @@
-import { View } from "react-native";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
-import Home from "../screens/Home";
-import Search from "../screens/Search";
-import Notifications from "../screens/Notifications";
-import Profile from "../screens/Profile";
+// import { View } from "react-native";
+// import { createAppContainer } from "react-navigation";
+// import { createStackNavigator } from "react-navigation-stack";
+// import Home from "../screens/Home";
+// import Search from "../screens/Search";
+// import Notifications from "../screens/Notifications";
+// import Profile from "../screens/Profile";
 
-const TabNavigation = createBottomTabNavigator({
-  Home,
-  Search,
-  Add: {
-    screen: View,
-    navigationOptions: {
-      tabBarOnPress: () => {
-        console.log("Add");
-      }
-    }
-  },
-  Notifications,
-  Profile
-});
+// const TabNavigation = createBottomTabNavigator({
+//   Home,
+//   Search,
+//   Add: {
+//     screen: View,
+//     navigationOptions: {
+//       tabBarOnPress: () => {
+//         console.log("Add");
+//       }
+//     }
+//   },
+//   Notifications,
+//   Profile
+// });
 
-export default createAppContainer(TabNavigation);
+// export default createAppContainer(TabNavigation);
